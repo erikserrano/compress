131e8dded80a39fba7fca007d62f9abdd69738bc
Serrano <Erik.Serrano@nielsen.com>
2016-11-16 18:53:26 -0600

    TESTING: UNZIP > RACE

--- unzip/unzip_test.go
+++ unzip/unzip_test.go
@@ -1,6 +1,7 @@
 package unzip
 
 import "testing"
+import "sync"
 
 // TestUnzip test Unzip function
 func TestUnzip(t *testing.T) {
@@ -12,11 +13,27 @@ func TestUnzip(t *testing.T) {
 	}
 }
 
+// BenchmarkUnzip benchmark Unzip function
 func BenchmarkUnzip(b *testing.B) {
-	files, err := Unzip("C:\\Users\\serrer01\\Downloads\\RR\\go\\zips\\I_07044_032_2016010_000510.zip", "C:\\Users\\serrer01\\Downloads\\DROP\\")
-	if err != nil {
-		b.Fatal(err)
-	} else if len(files) != 56 {
-		b.Fatalf("Expected %v items but got %v", 56, len(files))
+	files := []string{
+		"C:\\Users\\serrer01\\Downloads\\RR\\go\\zips\\I_07044_032_2016010_000510.zip",
+		"C:\\Users\\serrer01\\Downloads\\RR\\go\\zips\\I_07044_032_2016010_000511.zip",
+		"C:\\Users\\serrer01\\Downloads\\RR\\go\\zips\\I_07044_032_2016010_000513.zip",
+		"C:\\Users\\serrer01\\Downloads\\RR\\go\\zips\\I_07044_032_2016010_000514.zip",
+		"C:\\Users\\serrer01\\Downloads\\RR\\go\\zips\\I_07044_032_2016010_000517.zip",
 	}
+	var wg sync.WaitGroup
+
+	for _, path := range files {
+		wg.Add(1)
+		go func(sipPath string) {
+			defer wg.Done()
+
+			_, err := Unzip(sipPath, "C:\\Users\\serrer01\\Downloads\\DROP\\")
+			if err != nil {
+				b.Fatal(err)
+			}
+		}(path)
+	}
+	wg.Wait()
 }
